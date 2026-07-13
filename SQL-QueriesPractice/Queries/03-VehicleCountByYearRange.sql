fcab5db5a611efd980ada6224a508c1d6194b87a
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-13 14:48:35 +0300

    feat: add Vehicle Count By Make

--- SQL-QueriesPractice/Queries/03-VehicleCountByYearRange.sql
+++ SQL-QueriesPractice/Queries/03-VehicleCountByYearRange.sql
@@ -1,5 +1,11 @@
 /**
 Problem 3:
 
-Get number vehicles made betoeen 1950 and 2000 
+Get number vehicles made between 1950 and 2000 
 **/
+USE VehicleMakesDB;
+GO
+
+SELECT COUNT(*) AS NumberOfVehicles
+FROM vwVehicleMasterView
+WHERE Year BETWEEN 1950 AND 2000;
