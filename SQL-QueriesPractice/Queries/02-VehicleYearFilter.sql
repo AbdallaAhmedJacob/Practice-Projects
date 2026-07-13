66e0278a5e68cdec344192e945a4a78b914e895b
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-13 14:47:03 +0300

    feat: add Vehicle Year Filter

--- SQL-QueriesPractice/Queries/02-VehicleYearFilter.sql
+++ SQL-QueriesPractice/Queries/02-VehicleYearFilter.sql
@@ -1,5 +1,12 @@
 /**
 Problem 2:
 
-Get all vehicles made betoeen 1950 and 2000
+Get all vehicles made between 1950 and 2000
 **/
+USE VehicleMakesDB;
+GO
+
+SELECT * 
+FROM vwVehicleMasterView
+WHERE
+	Year BETWEEN  1950 AND 2000;
