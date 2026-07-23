04bdfaa11bc34c2aaeef245a32bda62c770d8f18
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-23 15:14:20 +0300

    feat: add a function to pass the contact from DB layer to Presentation layer

---
+++ Projects/Contacts/ContactsBusiness/Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using ContactsDataAccess;
+
+namespace ContactsBusiness
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string FirstNam { get; set; }
+        public string LastNam { get; set; } 
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int CountryID { get; set; }
+
+        Contact(int contactID, string firstNam, string lastNam, string email, string phone, int countryID)
+        {
+            ContactID = contactID;
+            FirstNam = firstNam;
+            LastNam = lastNam;
+            Email = email;
+            Phone = phone;
+            CountryID = countryID;
+        }
+
+        public static Contact Find(int ID)
+        {
+            string FirstName = "", LastName = "", Email = "", Phone = "";
+            int CountryID = -1;
+
+            if(ContactData.GetContactInfoByID(ref ID, ref FirstName, ref LastName, ref Email, ref Phone, ref CountryID))             
+            {
+                return new Contact(ID, FirstName, LastName, Email, Phone, CountryID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
