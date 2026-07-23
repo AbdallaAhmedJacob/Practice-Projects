9c40a11322f0d21792538a5a7ea5c07a1a328fac
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-23 15:29:59 +0300

    feat: add find contact screen to disblay contact details

---
+++ Projects/Contacts/ContactsConsoleApp/Screens/FindContactScreen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using ContactsBusiness;
+
+namespace ContactConsoleApp.Screens
+{
+    internal class FindContactScreen : BaseScreen
+    {
+        public static void ShowFindContactScreen()
+        {
+            BaseScreen.Header("Find Contact");
+            string con = "n";
+
+            do
+            {
+                Console.Write("\n\t\t\t\t\t Enter ID: ");
+                var ID = Console.ReadLine();
+                // validation integer.
+                Contact contact = Contact.Find(int.Parse(ID));
+                if (contact != null)
+                {
+                    Console.WriteLine($"\t\t\t\t\t______________________________________");
+                    Console.WriteLine($"\t\t\t\t\t ID        |{contact.ContactID}");
+                    Console.WriteLine($"\t\t\t\t\t Name      |{contact.FirstNam + " " + contact.LastNam}");
+                    Console.WriteLine($"\t\t\t\t\t Email     |{contact.Email}");
+                    Console.WriteLine($"\t\t\t\t\t Phone     |{contact.Phone}");
+                    Console.WriteLine($"\t\t\t\t\t Country ID|{contact.CountryID}");
+                    Console.WriteLine($"\t\t\t\t\t______________________________________");
+                }
+                else
+                {
+                    Console.WriteLine($"\t\t\t\t\t______________________________________");
+                    Console.WriteLine($"\t\t\t\t\tContact {ID} is not found :(");
+                    Console.WriteLine($"\t\t\t\t\t______________________________________");
+                }
+
+                Console.Write("\t\t\t\t\tDo you want to find another ID? [Y/N] ");
+                con = Console.ReadLine();
+            } while (con == "y" || con == "Y");
+        }
+    }
+}
