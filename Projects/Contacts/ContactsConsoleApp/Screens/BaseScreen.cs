d94bcc1faddd523132c6763616f41fa5b71e1223
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-23 15:26:15 +0300

    feat: add a base screen to be inherited by all screens for centralized cntrol

---
+++ Projects/Contacts/ContactsConsoleApp/Screens/BaseScreen.cs
@@ -0,0 +1,18 @@
+﻿namespace ContactConsoleApp.Screens
+{
+    internal class BaseScreen
+    {
+        protected static void Header(string Title, string SubTitle = "")
+        {
+            Console.WriteLine($"\t\t\t\t\t______________________________________");
+            Console.WriteLine($"\n\n\t\t\t\t\t  {Title}");
+            if (SubTitle != "")
+            {
+                Console.WriteLine($"\n\t\t\t\t\t  {SubTitle}");
+            }
+
+            Console.WriteLine($"\n\t\t\t\t\tDate: {DateTime.Now}");
+            Console.WriteLine($"\n\t\t\t\t\t______________________________________\n\n");
+        }
+    }
+}
