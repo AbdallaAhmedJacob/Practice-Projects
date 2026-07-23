e7a150bbd2cfe608ffee247fe74fd133582136e5
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-23 15:33:40 +0300

    feat: add Program file

---
+++ Projects/Contacts/ContactsConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ContactConsoleApp.Screens;
+using ContactsBusiness;
+
+namespace ContactConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindContactScreen.ShowFindContactScreen();
+        }
+    }
+}
