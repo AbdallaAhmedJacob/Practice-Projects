bc2fd692b45f36f0b4afb8e5dcd96b6d1f7c6a2f
AbdallaAhmedJacob <abdalla.ahmed.jacob@gmail.com>
2026-07-26 22:26:22 +0300

    feat: add a static function print contact info

--- Projects/Contacts/ContactsConsoleApp/Screens/BaseScreen.cs
+++ Projects/Contacts/ContactsConsoleApp/Screens/BaseScreen.cs
@@ -1,8 +1,10 @@
-﻿namespace ContactConsoleApp.Screens
+﻿using ContactsBusiness;
+
+namespace ContactConsoleApp.Screens
 {
     internal abstract class BaseScreen
     {
-        protected abstract void Header(string Title, string SubTitle = "")
+        protected static void Header(string Title, string SubTitle = "")
         {
             Console.WriteLine($"\t\t\t\t\t______________________________________");
             Console.WriteLine($"\n\n\t\t\t\t\t  {Title}");
@@ -14,5 +16,17 @@
             Console.WriteLine($"\n\t\t\t\t\tDate: {DateTime.Now}");
             Console.WriteLine($"\n\t\t\t\t\t______________________________________\n\n");
         }
+        public static void PrintContactInfo(Contact contact)
+        {
+            if (contact == null) return;
+
+            Console.WriteLine($"\t\t\t\t\t______________________________________");
+            Console.WriteLine($"\t\t\t\t\t ID        |{contact.ContactID}");
+            Console.WriteLine($"\t\t\t\t\t Name      |{contact.FirstNam + " " + contact.LastNam}");
+            Console.WriteLine($"\t\t\t\t\t Email     |{contact.Email}");
+            Console.WriteLine($"\t\t\t\t\t Phone     |{contact.Phone}");
+            Console.WriteLine($"\t\t\t\t\t Country ID|{contact.CountryID}");
+        }
+
     }
 }
